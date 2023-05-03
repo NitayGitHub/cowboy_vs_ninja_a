9e5dfd576860bedb3909b9eb3037ed9de926688e
nitayGitHub <nitaylevi1998@gmail.com>
2023-05-03 11:11:16 +0300

    PointFiles

---
+++ sources/Point.hpp
@@ -0,0 +1,28 @@
+#ifndef POINT_H
+#define POINT_H
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+using namespace std;
+
+namespace ariel
+{
+    class Point
+    {
+    private:
+        double x, y;
+
+    public:
+        Point(double x, double y);
+        double distance(const Point &other) const;
+        void print() const;
+        Point moveTowards(const Point &source, const Point &dest, double dist) const;
+
+        // getters and setters
+        double getX() const;
+        double getY() const;
+        void setX(double x);
+        void setY(double y);
+    };
+}
+#endif
